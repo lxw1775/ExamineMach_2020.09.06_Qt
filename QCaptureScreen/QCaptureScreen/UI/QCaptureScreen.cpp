fb45c32238ba39231eb2e831695210341ced1ad6
lxw <875121629@qq.com>
2020-09-05 11:55:43 +0800

    【新增】 去除标题栏

--- QCaptureScreen/QCaptureScreen/UI/QCaptureScreen.cpp
+++ QCaptureScreen/QCaptureScreen/UI/QCaptureScreen.cpp
@@ -1,7 +1,44 @@
 #include "QCaptureScreen.h"
+#include <QMouseEvent>
+
 
 QCaptureScreen::QCaptureScreen(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	//不显示标题栏
+	setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Dialog);
+}
+
+
+void QCaptureScreen::mouseMoveEvent(QMouseEvent* event)
+{
+	if (m_bDrag)
+	{
+		//获取鼠标移动的距离
+		QPoint pt = event->globalPos() - m_ptMouseStartPoint;
+		//改变窗口的位置
+		this->move(m_ptWindowTopleftPoint + pt);
+	}
+
+}
+
+void QCaptureScreen::mousePressEvent(QMouseEvent* event)
+{
+	if (event->button() == Qt::LeftButton)
+	{
+		m_bDrag = true;
+		//获取鼠标的位置
+		m_ptMouseStartPoint = event->globalPos();
+		//改变窗口的初始位置
+		m_ptWindowTopleftPoint = this->frameGeometry().topLeft();
+	}
+}
+
+void QCaptureScreen::mouseReleaseEvent(QMouseEvent* event)
+{
+	if (event->button() == Qt::LeftButton)
+	{
+		m_bDrag = false;
+	}
 }
--- QCaptureScreen/QCaptureScreen/UI/QCaptureScreen.h
+++ QCaptureScreen/QCaptureScreen/UI/QCaptureScreen.h
@@ -10,6 +10,18 @@ class QCaptureScreen : public QMainWindow
 public:
 	QCaptureScreen(QWidget *parent = Q_NULLPTR);
 
+
+protected:
+	//当不显示标题栏时 允许移动窗口
+	virtual void mousePressEvent(QMouseEvent*);
+	virtual void mouseMoveEvent(QMouseEvent*);
+	virtual void mouseReleaseEvent(QMouseEvent*);
+
 private:
 	Ui::QCaptureScreenClass ui;
+
+	//当不显示标题栏时 允许移动窗 变量
+	bool		m_bDrag;
+	QPoint		m_ptMouseStartPoint;
+	QPoint		m_ptWindowTopleftPoint;
 };
