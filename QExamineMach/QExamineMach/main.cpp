5d8ccdee5bf2a4da98252c14c326c7af76240db2
lxw1775 <875121629@qq.com>
2020-09-20 11:19:18 +0800

    【新增】加程序崩溃生成dump功能

--- QExamineMach/QExamineMach/main.cpp
+++ QExamineMach/QExamineMach/main.cpp
@@ -6,6 +6,10 @@
 #include <QDir>
 #include <QDebug>
 
+#include <tchar.h>
+#include <Windows.h>
+#include <DbgHelp.h>
+#pragma comment(lib, "user32.lib")
 
 void customMessageHandler(QtMsgType type, const QMessageLogContext& ctx, const QString& msg)
 {
@@ -49,15 +53,87 @@ void customMessageHandler(QtMsgType type, const QMessageLogContext& ctx, const Q
 }
 
 
+
+
+int GenerateMiniDump(PEXCEPTION_POINTERS pExceptionPointers)
+{
+	// 定义函数指针
+	typedef BOOL(WINAPI* MiniDumpWriteDumpT)(
+		HANDLE,
+		DWORD,
+		HANDLE,
+		MINIDUMP_TYPE,
+		PMINIDUMP_EXCEPTION_INFORMATION,
+		PMINIDUMP_USER_STREAM_INFORMATION,
+		PMINIDUMP_CALLBACK_INFORMATION
+		);
+	// 从 "DbgHelp.dll" 库中获取 "MiniDumpWriteDump" 函数
+	MiniDumpWriteDumpT pfnMiniDumpWriteDump = NULL;
+	HMODULE hDbgHelp = LoadLibrary(_T("DbgHelp.dll"));
+	if (NULL == hDbgHelp)
+	{
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	pfnMiniDumpWriteDump = (MiniDumpWriteDumpT)GetProcAddress(hDbgHelp, "MiniDumpWriteDump");
+
+	if (NULL == pfnMiniDumpWriteDump)
+	{
+		FreeLibrary(hDbgHelp);
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	// 创建 dmp 文件件
+	TCHAR szFileName[MAX_PATH] = { 0 };
+	TCHAR szVersion[] = L"DumpFile";
+	SYSTEMTIME stLocalTime;
+	GetLocalTime(&stLocalTime);
+	wsprintf(szFileName, L"dump/%s-%04d%02d%02d-%02d%02d%02d.dmp",
+		szVersion, stLocalTime.wYear, stLocalTime.wMonth, stLocalTime.wDay,
+		stLocalTime.wHour, stLocalTime.wMinute, stLocalTime.wSecond);
+	HANDLE hDumpFile = CreateFile(szFileName, GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+	if (INVALID_HANDLE_VALUE == hDumpFile)
+	{
+		FreeLibrary(hDbgHelp);
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	// 写入 dmp 文件
+	MINIDUMP_EXCEPTION_INFORMATION expParam;
+	expParam.ThreadId = GetCurrentThreadId();
+	expParam.ExceptionPointers = pExceptionPointers;
+	expParam.ClientPointers = FALSE;
+	pfnMiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
+		hDumpFile, MiniDumpWithDataSegs, (pExceptionPointers ? &expParam : NULL), NULL, NULL);
+	// 释放文件
+	CloseHandle(hDumpFile);
+	FreeLibrary(hDbgHelp);
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
+LONG WINAPI ExceptionFilter(LPEXCEPTION_POINTERS lpExceptionInfo)
+{
+	// 这里做一些异常的过滤或提示
+	if (IsDebuggerPresent()) {
+		return EXCEPTION_CONTINUE_SEARCH;
+	}
+	return GenerateMiniDump(lpExceptionInfo);
+}
+
+
+
 int main(int argc, char *argv[])
 {
-    QDir dir(QDir::currentPath() + QString("/Log/"));
-    if (!dir.exists())
+    QDir dir;
+    if (!dir.exists(QDir::currentPath() + QString("/Log/")))
         bool ok = dir.mkpath(QDir::currentPath() + QString("/Log/"));//创建多级目录
+	if (!dir.exists(QDir::currentPath() + QString("/dump/")))
+		bool ok = dir.mkpath(QDir::currentPath() + QString("/dump/"));//创建多级目录
 
     //加入打印日志
     qInstallMessageHandler(customMessageHandler);
     qInfo() << " start. "; //输出日志
+
+	//dump设置
+	SetUnhandledExceptionFilter(ExceptionFilter);
 
     QApplication a(argc, argv);
 
